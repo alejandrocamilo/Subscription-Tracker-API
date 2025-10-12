6fed8dc2e522798ed25f901a01b73f189670caa5
Alejandro Camilo <alejandrocamilojavier5@gmail.com>
2025-10-12 14:39:35 -0400

    adding authentication features in order to sign up and sign in successfully

---
+++ controllers/auth.controller.js
@@ -0,0 +1,103 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {JWT_EXPIRES_IN, JWT_SECRET} from "../config/env.js";
+
+// What is a req body? req.body is an object containing data coming from the client (Post RequestS)
+
+export const signUp = async (req, res, next) => {
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try{
+        // Logic to create a new user
+
+        const {name, email, password } = req.body;
+
+        // Check if the user already exists
+
+        const existingUser = await User.findOne({ email });
+
+        if(existingUser){
+            const error = new Error("User already exists");
+            error.statusCode = 409;
+            throw error;
+        }
+
+        // hash password
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create([{name, email, password: hashedPassword}], {session})
+
+        const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN} );
+
+        await session.commitTransaction();
+
+        await session.endSession();
+
+        res.status(201).json(
+            {
+                 success: true,
+                 message: "User signed up successfully",
+                 data: {
+                    token,
+                    user: newUsers[0],
+                 }
+            }
+        );
+    }
+    catch(error){
+
+       await session.abortTransaction();
+       await session.endSession();
+       next(error);
+    }
+}
+
+
+
+export const signIn = async (req, res, next) => {
+
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPaswordValid  = await bcrypt.compare(password, user.password);
+
+        if(!isPaswordValid){
+            const error = new Error("Invalid Password");
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        res.status(200).json({
+            success: true,
+            message: "User signed in successfully",
+            data: {
+                token,
+                user
+            }
+        })
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const signOut = async (req, res, next) => {
+
+}
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -2,7 +2,6 @@ const errorMiddleware = (err, req, res, next) => {
 
     try {
 
-
         let error = {...err}
 
         error.message = err.message;
