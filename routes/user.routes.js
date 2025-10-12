2773b252b9a03cd7325a1e3041379bfcd892ea3c
Alejandro Camilo <alejandrocamilojavier5@gmail.com>
2025-10-12 14:48:50 -0400

    adding authorization just to allow specific users to make certain API calls.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
+import {getUsers, getUser} from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 
 const userRouter = Router();
 
-userRouter.get("/", getUsers)
-userRouter.get("/:id", getUser)
+userRouter.get("/",authorize, getUsers)
+userRouter.get("/:id",authorize, getUser)
 
 userRouter.post("/", (req, res) => res.send({title: "CREATE a new user"}))
 
